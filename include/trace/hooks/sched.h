272be3ca0a31f7332190ee639082b394658a8604
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-14 18:30:47 +0800

    [vendor_hooks] adding cpu capacity vh

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -29,6 +29,10 @@ DECLARE_HOOK(android_vh_scheduler_tick,
 	TP_PROTO(struct rq *rq),
 	TP_ARGS(rq));
 
+DECLARE_RESTRICTED_HOOK(android_rvh_update_cpu_capacity,
+	TP_PROTO(int cpu, unsigned long *capacity),
+	TP_ARGS(cpu, capacity), 1);
+
 DECLARE_RESTRICTED_HOOK(android_rvh_enqueue_task,
 	TP_PROTO(struct rq *rq, struct task_struct *p),
 	TP_ARGS(rq, p), 1);
--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8956,6 +8956,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	if (!capacity)
 		capacity = 1;
 
+	trace_android_rvh_update_cpu_capacity(cpu, &capacity);
 	cpu_rq(cpu)->cpu_capacity = capacity;
 	sdg->sgc->capacity = capacity;
 	sdg->sgc->min_capacity = capacity;
